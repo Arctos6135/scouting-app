f1bc939961571c7e232a3c68a512b7db1a5f7f40
Alex Waese-Perlman <squishybanana04@gmail.com>
2019-02-17 18:49:07 -0500

    Use main screen

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './src/mainScreen'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<MainScreen></MainScreen>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 20
+	},
 });
