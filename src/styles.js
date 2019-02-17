9e30d2d389f81b62ccbb26009502e5c4b2b9b2b2
Alex Waese-Perlman <squishybanana04@gmail.com>
2019-02-17 18:47:55 -0500

    Add font, alignment and input styles

---
+++ src/styles.js
@@ -0,0 +1,51 @@
+export default styles = {
+	colors: {
+		highlight: {
+			bg: "#78D3E1",
+			text: "#100E0C"
+		},
+		secondary: {
+			bg: "#cccccc",
+			text: "#000000"
+		}
+	},
+	shadow: {
+		shadowColor: '#000000',
+		shadowOffset: {
+			width: 0,
+			height: 3
+		},
+		shadowRadius: 5,
+		shadowOpacity: 1.0,
+		elevation: 5,
+	},
+	font: {
+		standardText: {
+			fontSize: 18
+		},
+		header: {
+			fontSize: 24
+		},
+		inputHeader: {
+			fontSize: 18
+		},
+		navButton: {
+			fontSize: 18
+		}
+	},
+	align: {
+		center: {
+			textAlign: "center",
+			justifyContent: "center",
+			alignItems: "center"
+		}
+	},
+	inputs: {
+		inputBox: {
+			padding: 3,
+			borderRadius: 0,
+			borderWidth: 0.5,
+			borderColor: '#d6d7da',
+		}
+	}
+}
