e30a4810c03146a2f178e2fcec041accfe197853
Alex Waese-Perlman <squishybanana04@gmail.com>
2019-02-20 00:56:45 -0500

    Add slider inputs for tele-op section

--- src/dataEntry.js
+++ src/dataEntry.js
@@ -40,21 +40,41 @@ const dataEntryStyles = {
 }
 
 const Row = (props) =>
-	(<View style={{
-		...props.style,
+	(<View style={[{
 		width: "100%",
-		paddingBottom: 20,
+		paddingBottom: 10,
 		justifyContent: "space-between",
 		alignItems: "center",
 		flexDirection: "row"
-	}}>{props.children}
+	}, props.style]}>{props.children}
 	</View>);
 const Spacer = (props) => (<View style={{ flex: 0.1 }}></View>);
 
 const dataNames = {
 	cargo: ["cargo1", "cargo2", "cargo3", "cargo4"],
-	hatch: ["hatch1", "hatch2", "hatch3", "hatch4"]
+	hatch: ["hatch1", "hatch2", "hatch3", "hatch4"],
+	rocketCargo: ["rock1c", "rock2c", "rock3c", "rock4c"],
+	rocketHatch: ["rock1h", "rock2h", "rock3h", "rock4h"],
+	climbing: {
+		levelReached: "climblvlReached",
+		assist: "assist"
+	}
 }
+
+const climbOptions = [
+	"No climb",
+	"Level 1",
+	"Level 2",
+	"Level 3"
+]
+const defaultClimbOption = 0;
+
+const assistOptions = [
+	"No assist",
+	"Level 2",
+	"Level 3"
+]
+const defaultAssistOption = 0;
 
 const gamePieceOptions = [
 	"None",
@@ -76,6 +96,15 @@ export default class DataEntry extends React.Component {
 			if (!newData[dataNames.cargo[i]]) newData[dataNames.cargo[i]] = gamePieceOptions[defaultGamePieceOption];
 			if (!newData[dataNames.hatch[i]]) newData[dataNames.hatch[i]] = gamePieceOptions[defaultGamePieceOption];
 		}
+
+		for (let i in dataNames.rocket) {
+			if (!newData[dataNames.rocket[i]]) newData[dataNames.rocket[i]] = 0;
+		}
+
+		if (!newData[dataNames.climbing.levelReached]) newData[dataNames.climbing.levelReached] = climbOptions[defaultClimbOption];
+		if (!newData[dataNames.climbing.assist]) newData[dataNames.climbing.assist] = assistOptions[defaultAssistOption];
+
+
 		this.props.onDataChange(newData);
 		this.originalValue = this.props.data;
 	}
@@ -98,10 +127,11 @@ export default class DataEntry extends React.Component {
 		this.props.return();
 	}
 	render() {
-		let rockets = [];
+		console.log(this.props.data);
+		let sandstormRockets = [];
 		for (let i = 0; i < dataNames.cargo.length; i++) {
-			rockets.push(<Row key={i}>
-				<inputs.LabeledInput label={"Hatch " + (i + 1)} style={dataEntryStyles.gamePieceInput}>
+			sandstormRockets.push(<Row key={i}>
+				<inputs.LabeledInput textStyle={styles.font.dataEntry} label={"Hatch " + (i + 1)} style={dataEntryStyles.gamePieceInput}>
 					<inputs.PickerInput value={this.props.data[dataNames.hatch[i]]} options={gamePieceOptions}
 						onValueChange={(selected) => this.dataUpdated(selected, dataNames.hatch[i])}
 						style={{
@@ -114,7 +144,7 @@ export default class DataEntry extends React.Component {
 
 				<Spacer></Spacer>
 
-				<inputs.LabeledInput label={"Cargo " + (i + 1)} style={dataEntryStyles.gamePieceInput}>
+				<inputs.LabeledInput textStyle={styles.font.dataEntry}  label={"Cargo " + (i + 1)} style={dataEntryStyles.gamePieceInput}>
 					<inputs.PickerInput value={this.props.data[dataNames.cargo[i]]} options={gamePieceOptions}
 						onValueChange={(selected) => this.dataUpdated(selected, dataNames.cargo[i])}
 						style={{
@@ -126,6 +156,57 @@ export default class DataEntry extends React.Component {
 				</inputs.LabeledInput>
 			</Row>)
 		}
+
+		let teleopRockets = [];
+		let key = 0;
+		for (let i = 0; i < dataNames.rocketCargo.length; i++) {
+			teleopRockets.push(<Row key={key++}>
+				<Text style={[styles.font.subHeader]}>Rocket {(i + 1).toString()}</Text>
+			</Row>)
+			teleopRockets.push(<Row style={{paddingBottom: 5}} key={key++}>
+				<inputs.LabeledInput textStyle={styles.font.dataEntry}  label={"Cargo"} style={dataEntryStyles.gamePieceInput}>
+					<inputs.SliderInput step={1} minimumValue={0} maximumValue={3} value={this.props.data[dataNames.rocketCargo[i]]} options={gamePieceOptions}
+						onValueChange={(value) => this.dataUpdated(value, dataNames.rocketCargo[i])}
+					></inputs.SliderInput>
+				</inputs.LabeledInput>
+			</Row>);
+
+			teleopRockets.push(<Row style={{ paddingBottom: 5 }} key={key++}>
+				<inputs.LabeledInput textStyle={styles.font.dataEntry} label={"Hatch"} style={dataEntryStyles.gamePieceInput}>
+					<inputs.SliderInput step={1} minimumValue={0} maximumValue={3} value={this.props.data[dataNames.rocketHatch[i]]} options={gamePieceOptions}
+						onValueChange={(value) => this.dataUpdated(value, dataNames.rocketHatch[i])}
+					></inputs.SliderInput>
+				</inputs.LabeledInput>
+			</Row>);
+		}
+
+		let climbing = (
+			<Row>
+				<inputs.LabeledInput textStyle={styles.font.dataEntry} label={"Level reached"} style={dataEntryStyles.gamePieceInput}>
+					<inputs.PickerInput value={this.props.data[dataNames.climbing.levelReached]} options={climbOptions}
+						onValueChange={(selected) => this.dataUpdated(selected, dataNames.climbing.levelReached)}
+						style={{
+							backgroundColor:
+								this.props.data[dataNames.climbing.levelReached] == climbOptions[defaultClimbOption] ?
+									styles.colors.tertiary.bg : styles.colors.secondary.bg
+						}}
+					></inputs.PickerInput>
+				</inputs.LabeledInput>
+
+				<Spacer></Spacer>
+
+				<inputs.LabeledInput textStyle={styles.font.dataEntry} label={"Assist"} style={dataEntryStyles.gamePieceInput}>
+					<inputs.PickerInput value={this.props.data[dataNames.climbing.assist]} options={assistOptions}
+						onValueChange={(selected) => this.dataUpdated(selected, dataNames.climbing.assist)}
+						style={{
+							backgroundColor:
+								this.props.data[dataNames.climbing.assist] == assistOptions[defaultAssistOption] ?
+									styles.colors.tertiary.bg : styles.colors.secondary.bg
+						}}
+					></inputs.PickerInput>
+				</inputs.LabeledInput>
+			</Row>
+		)
 		return (<View style={{ width: "100%", flex: 1, flexDirection:"column" }}>
 			{/* Submit and cancel buttons */}
 			<Row>
@@ -153,7 +234,7 @@ export default class DataEntry extends React.Component {
 			<View style={{ height: 30 }}></View>
 			<ScrollView>
 				<Row>
-					<MatchList matches={[this.props.data]} touchable={true}></MatchList>
+					<MatchList matches={[this.props.data]}></MatchList>
 				</Row>
 				<View style={{ height: 30 }}></View>
 				{/* Sandstorm phase */}
@@ -162,7 +243,22 @@ export default class DataEntry extends React.Component {
 						Sandstorm
 					</Text>
 				</Row>
-				{rockets}
+				{sandstormRockets}
+
+				<Row style={{paddingTop: 20}}>
+					<Text style={dataEntryStyles.header}>
+						Tele-op
+					</Text>
+				</Row>
+				{teleopRockets}
+
+				<Row style={{ paddingTop: 20 }}>
+					<Text style={dataEntryStyles.header}>
+						Climbing
+					</Text>
+				</Row>
+				{climbing}
+
 				<View style={{ height: 50 }}></View>
 				<Row>
 					<TouchableOpacity onPress={() => { this.delete() }} style={dataEntryStyles.controlBarButton}>
--- src/inputs.js
+++ src/inputs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import Slider from 'react-native-slider'
 import styles from './styles'
 import {
 	Menu,
@@ -14,20 +15,18 @@ export class NumberInput extends React.Component {
 	state = {
 		value: ""
 	}
+	number = 0;
 	onChanged(text) {
 		let newText = '';
-		let numbers = '0123456789';
+		let numbers = '0123456789' + this.props.allowFloat ? "." : "";
 
 		for (var i = 0; i < text.length; i++) {
 			if (numbers.indexOf(text[i]) > -1) {
 				newText = newText + text[i];
 			}
-			else {
-				alert("please enter numbers only");
-			}
 		}
-		this.setState({ value: newText });
-		if (this.props.onValueChange) this.props.onValueChange(parseInt(newText));
+		number = parseFloat(newText);
+		if (this.props.onValueChange) this.props.onValueChange(parseInt(this.state.value));
 	}
 	render() {
 		return (
@@ -37,14 +36,14 @@ export class NumberInput extends React.Component {
 				onChangeText={(text) => this.onChanged(text)}
 				value={this.props.value ? this.props.value.toString() : this.state.value.toString()}
 				maxLength={this.props.maxLength}  //setting limit of input
-				returnKeyType='done' 
+				returnKeyType='done'
 			/>
 		)
 	}
 }
 
 
-export class PickerInput extends React.Component{
+export class PickerInput extends React.Component {
 	render() {
 		let current = 0;
 		return (
@@ -54,7 +53,7 @@ export class PickerInput extends React.Component{
 					padding: 10
 				}, this.props.style]} />
 				<MenuOptions>
-					{this.props.options ? 
+					{this.props.options ?
 						this.props.options.map((v) =>
 							<MenuOption style={[{ padding: 10 }, this.props.textStyle]} key={current++} value={v} text={v}></MenuOption>
 						)
@@ -64,10 +63,22 @@ export class PickerInput extends React.Component{
 	}
 }
 
+export class SliderInput extends React.Component {
+	render() {
+		return (
+			<View style={[this.props.style, {flexDirection: "row"}]}>
+				<Slider thumbStyle={{ backgroundColor: styles.colors.highlight.bg }} minimumTrackTintColor={styles.colors.highlight.bg} maximumTrackTintColor={styles.colors.tertiary.bg} step={this.props.step} minimumValue={this.props.minimumValue} maximumValue={this.props.maximumValue} value={this.props.value || 0} style={[{ flex: 1 }, this.props.sliderStyle]} onValueChange={(v) => this.props.onValueChange(v)}></Slider>
+				<View style={{ flex: 0.1 }}></View>
+				<NumberInput style={[{ flex: 0.25 }, this.props.inputStyle]} onChanged={(v) => this.props.onValueChange(v)} value={this.props.value || "0"} maxLength={this.props.maxLength}></NumberInput>
+			</View>
+		)
+	}
+}
+		
 export class LabeledInput extends React.Component {
 	render() {
 		return <View style={this.props.style}>
-			<Text style={[this.props.textStyle, {width: "100%", marginBottom: 2}]}>{this.props.label}</Text>
+			<Text style={[this.props.textStyle, { width: "100%", marginBottom: 2 }]}>{this.props.label}</Text>
 			{this.props.children}
 		</View>
 	}
