d348bafcff0e98be1adbba0d1f77c46260046800
Moeez Muhammad <mmoeez48@gmail.com>
2020-02-23 19:18:56 -0500

    Finish 2020 app

--- src/localDataMap.js
+++ src/localDataMap.js
@@ -254,24 +254,26 @@ function returnDataMap() {
 				{
 					"title": "Attributes",
 					"rows": [
-						{
-							"title": "Did the robot break?",
-							"type": "toggle",
-							"options": [
-								"No",
-								"Yes"
-							],
-							"id": "ab"
-						},
-						{
-							"title": "Did the robot tip?",
-							"type": "toggle",
-							"options": [
-								"No",
-								"Yes"
-							],
-							"id": "at"
-						},
+						[
+							{
+								"title": "Did the robot break?",
+								"type": "toggle",
+								"options": [
+									"No",
+									"Yes"
+								],
+								"id": "ab"
+							},
+							{
+								"title": "Did the robot tip?",
+								"type": "toggle",
+								"options": [
+									"No",
+									"Yes"
+								],
+								"id": "at"
+							},
+						],	
 						{
 							"title": "Can it pick up hatches from the floor?",
 							"type": "toggle",
@@ -631,6 +633,371 @@ function returnDataMap() {
 							],
 							"id": "adp" // auto dispense partner
 						},
+					]
+				},
+				{
+					"title": "Teleop",
+					"rows": [
+						{
+							"title": "Balls attempted from:",
+							"type": "text"
+						},
+
+						{
+							"title": "Close side of trench",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tactl" // teleop attempted close trench low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tacth" // teleop attempted close trench high
+							},
+						],
+
+						{
+							"title": "Far side of trench",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "taftl" // teleop attempted far trench low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tafth" // teleop attempted far trench high
+							},
+						],
+
+						{
+							"title": "Rendezvous",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tarl" // teleop attempted rendezvous low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tarh" // teleop attempted rendezvous high
+							},
+						],
+
+						{
+							"title": "Cross court",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "taccl" // teleop attempted cross court low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tacch" // teleop attempted cross court high
+							},
+						],
+
+
+						{
+							"title": "",
+							"type": "text"
+						},
+
+
+						{
+							"title": "Balls scored from:",
+							"type": "text"
+						},
+
+						{
+							"title": "Close side of trench",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsctl" // teleop scored close trench low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tscth" // teleop scored close trench high
+							},
+						],
+
+						{
+							"title": "Far side of trench",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsftl" // teleop scored far trench low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsfth" // teleop scored far trench high
+							},
+						],
+
+						{
+							"title": "Rendezvous",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsrl" // teleop scored rendezvous low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsrh" // teleop scored rendezvous high
+							},
+						],
+
+						{
+							"title": "Cross court",
+							"type": "text",
+						},
+						[
+							{
+								"title": "Low",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tsccl" // teleop scored cross court low
+							},
+							{
+								"title": "High",
+								"type": "number",
+								"increments": [
+									-1,
+									1
+								],
+								"id": "tscch" // teleop scored cross court high
+							},
+						],
+
+						{
+							"title": "Refuel from:",
+							"type": "text"
+						},
+						{
+							"title": "Ground",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "trg" // teleop refuel ground 
+						},
+						{
+							"title": "Higher level loading bay",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "trh" // teleop refuel high 
+						},
+
+						{
+							"title": "Did they dispense to alliance partners?",
+							"type": "picker",
+							"options": [
+								"No",
+								"Attempted",
+								"Successful"
+							],
+							"id": "tdp" // teleop dispense partner
+						},
+					]
+				},
+				{
+					"title": "Control Panel",
+					"rows": [
+						{
+							"title": "Spin?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "tps" // teleop panel spin
+						},
+						{
+							"title": "Position?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "tpp" // teleop panel position
+						}
+					]
+				},
+				{
+					"title": "Endgame",
+					"rows": [
+						[
+							{
+								"title": "Level",
+								"type": "picker",
+								"options": [
+									"None",
+									"Park",
+									"Hang"
+								],
+								"id": "el" // endgame level
+							},
+							{
+								"title": "Assist",
+								"type": "picker",
+								"options": [
+									"None",
+									"Was assisted",
+									"Assisted another"
+								],
+								"id": "ea" // endgame assist
+							}
+						],
+						{
+							"title": "Time from rendezvous to hook",
+							"type": "timer",
+							"id": "th" // time hook
+						},
+						{
+							"title": "Time from hook to fully lifted",
+							"type": "timer",
+							"id": "tl" // time lift
+						},
+						{
+							"title": "Leveling",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "lt" // leveling techniques
+						}
+					]
+				},
+				{
+					"title": "Attributes",
+					"rows": [
+						{
+							"title": "Can they outtake balls to pass?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "ao" // atrribute outtake
+						},
+						{
+							"title": "Can they drive under the trench?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "at" // atrribute trench
+						},
+						{
+							"title": "Did they break?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "ab" // attribute break
+						},
+						{
+							"title": "Did they tip?",
+							"type": "toggle",
+							"options": [
+								"No",
+								"Yes"
+							],
+							"id": "at" // attribute tip
+						}
 					]
 				}
 			]
